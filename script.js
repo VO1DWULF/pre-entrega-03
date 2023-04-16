b25098b89cf30ab422ef6ce8489405cef6affb06
marco valencia <mmvb67@gmail.com>
2023-04-16 19:58:58 -0400

    primer commit pre entrega 3 curso javascript

---
+++ script.js
@@ -0,0 +1,361 @@
+let mangaList = [
+  {
+    id: 1,
+    name: "One Piece",
+    price: 10,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/71y+XnBXm4L._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 2,
+    name: "Naruto",
+    price: 12,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/71QYLrc-IQL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 3,
+    name: "Dragon Ball",
+    price: 11,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/71l53JXXQ3L._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 4,
+    name: "Bleach",
+    price: 14,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/51FEKMNJTbL.jpg"
+},
+{
+    id: 5,
+    name: "Attack on Titan",
+    price: 18,
+    age: 18,
+    genre: "seinen",
+    image: "https://m.media-amazon.com/images/I/91M9VaZWxOL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 6,
+    name: "Death Note",
+    price: 15,
+    age: 18,
+    genre: "seinen",
+    image: "https://m.media-amazon.com/images/I/51SkSDhUidL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 7,
+    name: "Fullmetal Alchemist",
+    price: 13,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/61GWN9NPJvL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 8,
+    name: "My Hero Academia",
+    price: 16,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/51FAgOL-1bL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 9,
+    name: "Demon Slayer",
+    price: 17,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/81ZNkhqRvVL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 10,
+    name: "Evangelion",
+    price: 14,
+    age: 18,
+    genre: "seinen",
+    image: "https://m.media-amazon.com/images/I/51RZYiVyRFL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 11,
+    name: "Berserk",
+    price: 15,
+    age: 18,
+    genre: "seinen",
+    image: "https://m.media-amazon.com/images/I/91D07epNE9L._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 12,
+    name: "Given",
+    price: 16,
+    age: 18,
+    genre: "yaoi",
+    image: "https://m.media-amazon.com/images/I/71qdwc4c3pL.jpg"
+},
+{
+    id: 13,
+    name: "Blue Lock",
+    price: 15,
+    age: 14,
+    genre: "sports",
+    image: "https://m.media-amazon.com/images/I/51jMLFs0YBL.jpg"
+},
+{
+    id: 14,
+    name: "Orange",
+    price: 16,
+    age: 14,
+    genre: "drama",
+    image: "https://m.media-amazon.com/images/I/A1IcRL4BbmL._SL1500_.jpg"
+},
+{
+    id: 15,
+    name: "Spy x Family",
+    price: 17,
+    age: 14,
+    genre: "drama",
+    image: "https://m.media-amazon.com/images/I/71vMGRog+iL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 16,
+    name: "Hunter x Hunter",
+    price: 12,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/51muOXJRRgL.jpg"
+},
+{
+    id: 17,
+    name: "Tokyo Ghoul",
+    price: 16,
+    age: 14,
+    genre:"seinen",
+    image: "https://m.media-amazon.com/images/I/51uWQfEYayL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 18,
+    name: "Banana Fish",
+    price: 15,
+    age: 18,
+    genre:"drama",
+    image: "https://m.media-amazon.com/images/I/81jSY-3rQRL.jpg"
+},
+{
+    id: 19,
+    name: "Hajime no Ippo",
+    price: 12,
+    age: 14,
+    genre: "sports",
+    image: "https://m.media-amazon.com/images/I/71B6gjmehuL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 20,
+    name: "One Punch Man",
+    price: 14,
+    age: 14,
+    genre: "shonen",
+    image: "https://m.media-amazon.com/images/I/81VAgJoB3BL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+    id: 21,
+    name: "Slam Dunk",
+    price: 15,
+    age: 14,
+    genre: "sports",
+    image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81hF7CIHUTL._AC_UF1000,1000_QL80_.jpg"
+},
+{
+  id: 22,
+  name: "Killing Stalking",
+  price: 17,
+  genre: "yaoi",
+  age: 18,
+  image: "https://m.media-amazon.com/images/I/71GOjN7YY4L.jpg",
+},
+{
+    id: 23,
+    name: "Gantz",
+    price: 16,
+    age: 18,
+    genre: "seinen",
+    image: "https://m.media-amazon.com/images/I/41vP2ZtqYfL._AC_UF1000,1000_QL80_.jpg"
+},
+  {
+      id: 24,
+      name: "Ping Pong",
+      price: 12,
+      genre: "sports",
+      age: 14,
+      image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71BfVY5YEZL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+      id: 25,
+      name: "Hellsing",
+      price: 16,
+      genre: "seinen",
+      age: 18,
+      image: "https://m.media-amazon.com/images/I/91lighLBbkL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+      id: 26,
+      name: "Vinland Saga",
+      price: 15,
+      genre: "shonen",
+      age: 14,
+      image: "https://m.media-amazon.com/images/I/91KS3nAZfRL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+      id: 27,
+      name: "Oyasumi Punpun",
+      price: 16,
+      genre: "drama",
+      age: 18,
+      image: "https://m.media-amazon.com/images/I/61P35IvoVFL.jpg",
+  },
+];
+
+const genreFilter = document.getElementById("genre-filter");
+const priceSort = document.getElementById("price-sort");
+
+genreFilter.addEventListener("change", updateMangaList);
+priceSort.addEventListener("change", updateMangaList);
+
+function updateMangaList() {
+
+  const genreValue = genreFilter.value;
+  const priceValue = priceSort.value;
+
+  let filteredList = mangaList;
+  if (genreValue) {
+      filteredList = filteredList.filter((manga) => manga.genre === genreValue);
+  }
+
+  if (priceValue === "asc") {
+      filteredList.sort((a, b) => a.price - b.price);
+  } else if (priceValue === "desc") {
+      filteredList.sort((a, b) => b.price - a.price);
+  }
+
+  const mangaListContainer = document.getElementById("manga-list");
+
+  mangaListContainer.innerHTML = "";
+
+  filteredList.forEach((manga) => {
+      const mangaItem = document.createElement("div");
+      mangaItem.className = "manga-item";
+      mangaItem.innerHTML = `
+          <img src="${manga.image}" alt="${manga.name}">
+          <h2>${manga.name}</h2>
+          <p>Price: $${manga.price}</p>
+          <p>Genre: ${manga.genre}</p>
+          <p>Age: ${manga.age}+</p>
+          <button onclick="addToCart(${manga.id})">Add to Cart</button>
+      `;
+      mangaListContainer.appendChild(mangaItem);
+  });
+
+  localStorage.setItem("filteredMangaList", JSON.stringify(filteredList));
+}
+
+updateMangaList();
+
+const cartItems = document.getElementById("cart-items");
+const checkoutButton = document.getElementById("checkout-button");
+
+let cart = [];
+const savedCart = localStorage.getItem("cart");
+if (savedCart) {
+cart = JSON.parse(savedCart);
+}
+
+function updateCart() {
+cartItems.innerHTML = "";
+let cartTotal = 0;
+cart.forEach((item) => {
+  const cartItem = document.createElement("div");
+  cartItem.className = "cart-item";
+  cartItem.innerHTML = `
+    <span>${item.name} - $${item.price}</span>
+    <button onclick="removeFromCart(${item.id})">Remove</button>
+  `;
+  cartItems.appendChild(cartItem);
+  cartTotal += item.price;
+});
+
+const cartTotalElement = document.createElement("div");
+cartTotalElement.innerHTML = `
+  <strong>Cart Total: $${cartTotal}</strong>
+`;
+cartItems.appendChild(cartTotalElement);
+}
+
+checkoutButton.addEventListener("click", function() {
+let cartTotal = 0;
+cart.forEach((item) => {
+  cartTotal += item.price;
+});
+alert(`Your total amount is: $${cartTotal}, Thanks for your for purchase!!`);
+});
+
+function addToCart(id) {
+const item = mangaList.find((manga) => manga.id === id);
+cart.push(item);
+localStorage.setItem("cart", JSON.stringify(cart));
+updateCart();
+}
+
+function removeFromCart(id) {
+const index = cart.findIndex((item) => item.id === id);
+if (index !== -1) {
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCart();
+}
+}
+
+const clearButton = document.getElementById("clear-button");
+clearButton.addEventListener("click", function() {
+cart = [];
+localStorage.removeItem("cart");
+updateCart();
+});
+
+let users = localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : [];
+
+const registerBtn = document.getElementById("register-btn");
+registerBtn.addEventListener("click", function() {
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+const userExists = users.find(function(user) {
+  return user.username === username;
+});
+
+if (userExists) {
+  alert("Username already taken!");
+} else {
+  const newUser = { username: username, password: password };
+  users.push(newUser);
+  localStorage.setItem("users", JSON.stringify(users));
+  alert("User registered successfully!");
+}
+});
+
+const loginBtn = document.getElementById("login-btn");
+loginBtn.addEventListener("click", function() {
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+const user = users.find(function(user) {
+  return user.username === username && user.password === password;
+});
+
+if (user) {
+  alert("Welcome back, " + user.username + "!");
+} else {
+  alert("Invalid username or password!");
+}
+});
